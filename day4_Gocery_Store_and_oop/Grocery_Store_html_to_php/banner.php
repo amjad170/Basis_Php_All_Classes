2314e3889fee566dbb5a773d7c74394bf3cf9f2a
Simanta pau <amjadhossain17416762@gmail.com>
2023-05-12 02:11:02 +0600

    html to php done

---
+++ day4_Gocery_Store_and_oop/Grocery_Store_html_to_php/banner.php
@@ -0,0 +1,46 @@
+<?php
+$title = "Grocery Store Website  | Banner";
+?>
+<!-- Header part -->
+<?php include("includes/header.php")?>
+
+<section class="about" id="about">
+
+
+<h1 class="heading"> our <span> banner</span> </h1>
+
+<div class="banner-container">
+
+<div class="banner">
+    <img src="images/banner1.png" alt="">
+    <div class="content">
+        <span>limited sales</span>
+        <h3>Vegetables</h3>
+        <a href="#" class="btn">shop now</a>
+    </div>
+</div>
+
+<div class="banner">
+    <img src="images/banner2.png" alt="">
+    <div class="content">
+        <span>limited sales</span>
+        <h3>fruits</h3>
+        <a href="#" class="btn">shop now</a>
+    </div>
+</div>
+
+<div class="banner">
+    <img src="images/banner3.png" alt="">
+    <div class="content">
+        <span>limited sales</span>
+        <h3>assorted</h3>
+        <a href="#" class="btn">shop now</a>
+    </div>
+</div>
+
+</div>
+</section>
+
+
+<!-- Footer part -->
+<?php require_once("includes/footer.php")?>
