ac2da237210b583bc8326019a790329af920c829
Simanta pau <amjadhossain17416762@gmail.com>
2023-05-11 09:30:58 +0600

    ok

--- day3/oop.php
+++ day3/oop.php
@@ -18,8 +18,10 @@ echo "Company Name: " . $Econo ->cname;
 $Econo ->pname = "Econo Full Time Ball Pen <br>";
 echo "Pen Name: " .  $Econo ->pname;
 $Econo ->pprice = 6;
-echo "Price: " . $Econo ->pprice." Tk";
+echo "Price: " . $Econo ->pprice." Tk <br>";
+$Econo->write();
 echo "<hr>";
+
 
 $Matador = new Pen();
 $Matador ->cname = "Matador <br>";
@@ -27,7 +29,8 @@ echo "Company Name: " . $Matador ->cname;
 $Matador ->pname = "MATADOR HI-SCHOOL <br>";
 echo "Pen Name: " .  $Matador ->pname;
 $Matador ->pprice = 5;
-echo "Price: " . $Matador ->pprice." Tk";
+echo "Price: " . $Matador ->pprice." Tk <br>";
+$Econo->write();
 echo "<hr>";
 
 $good_Luck = new Pen();
@@ -36,7 +39,8 @@ echo "Company Name: " . $good_Luck ->cname;
 $good_Luck ->pname = "Good Luck Craze Blako  <br>";
 echo "Pen Name: " .  $good_Luck ->pname;
 $good_Luck ->pprice = 10;
-echo "Price: " . $good_Luck ->pprice." Tk";
+echo "Price: " . $good_Luck ->pprice." Tk <br>";
+$Econo->write();
 echo "<hr>";
 
 $ATCross = new Pen();
@@ -45,7 +49,8 @@ echo "Company Name: " . $ATCross ->cname;
 $ATCross ->pname = "Cross Century II Ball  <br>";
 echo "Pen Name: " .  $ATCross ->pname;
 $ATCross ->pprice = 8835;
-echo "Price: " . $ATCross ->pprice." Rs";
+echo "Price: " . $ATCross ->pprice." Rs <br>";
+$Econo->write();
 echo "<hr>";
 
 $Parker = new Pen();
@@ -54,7 +59,8 @@ echo "Company Name: " . $Parker ->cname;
 $Parker ->pname = " Parker Premier Graduated Chiselling ST Fountain  <br>";
 echo "Pen Name: " .  $Parker ->pname;
 $Parker ->pprice = 21000;
-echo "Price: " . $Parker ->pprice." Rs";
+echo "Price: " . $Parker ->pprice." Rs <br>";
+$Econo->write();
 echo "<hr>";
 
 
